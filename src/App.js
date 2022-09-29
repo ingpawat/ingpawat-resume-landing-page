0b81234f05a078ca56430181538fe6a07eb6b0b5
Ingpawat <Ingpawat.k@gmail.com>
2022-09-29 10:29:09 +0700

    add backgroud

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from "./components/navbar";
 import React from 'react';
+import Wallpaper from './asset/water22.webp';
 
 function App() {
 
@@ -9,9 +10,13 @@ function App() {
             {/* Nav-bar */}
             <NavBar />
 
+            {/* banner */}
+            <img src={Wallpaper} alt="wallpaper" className='w-[100%] z-0' />
+            
+
             {/* content */}
             <div className='w-[100%] h-[4400px] p-0 m-0 bg-[#050505]'>
-
+                <h1 className='color-red'>Hello</h1>
             </div>
 
 
